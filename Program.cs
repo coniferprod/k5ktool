03a54c92980be85de6a19d1dc91eba107ad5fd98
Jere Käpyaho <jere@coniferproductions.com>
2018-08-14 09:53:55 +0300

    Created initial console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace k5ktool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Kawai K5000 world!");
+        }
+    }
+}
