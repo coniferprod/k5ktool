3e95a172c18fcb5fc6f93584d61c539216236e61
Jere Käpyaho <jere@coniferproductions.com>
2019-01-13 12:33:17 +0200

    Extracting name data from the patch

--- BankPlayground.playground/Contents.swift
+++ BankPlayground.playground/Contents.swift
@@ -5,6 +5,7 @@
 // and put the bank file(s) there.
 // See https://medium.com/@vhart/read-write-and-delete-file-handling-from-xcode-playground-abf57e445b4
 
+import Foundation
 import Cocoa
 import PlaygroundSupport
 
@@ -24,6 +25,12 @@ enum Bank {
     static let maxPatchCount = 128
     static let maxSourceCount = 6
     static let poolSize = 0x20000
+    static let commonDataSize = 82
+    static let sourceDataSize = 86
+    static let sourceCountOffset = 51
+    static let additiveWaveKitSize = 806
+    static let nameOffset = 40
+    static let nameSize = 8
 }
 
 // https://stackoverflow.com/a/47221437/1016326
@@ -42,6 +49,16 @@ struct PatchPointer {
     var index: Int
     var tonePointer: TonePointer
     var sourcePointers: [TonePointer]
+    
+    var additiveWaveKitCount: Int {
+        // The source pointer for any additive wave kit is non-zero.
+        // See https://www.hackingwithswift.com/example-code/language/how-to-count-matching-items-in-an-array
+        return sourcePointers.filter { $0 != 0 	}.count
+    }
+    
+    func isAdditive(sourceIndex: Int) -> Bool {
+        return sourcePointers[sourceIndex] != 0
+    }
 }
 
 var patches = [PatchPointer]()
@@ -108,8 +125,38 @@ for patch in patches {
     patchIndex += 1
 }
 
-for p in adjustedPatches {
+// Now we have all the adjusted data pointers, so we can start picking up
+// chunks of data from the big pool based on them.
+
+for (index, patch) in adjustedPatches.enumerated() {
+    /*
     print(String(format: "%03d - %08X", p.index, p.tonePointer))
     print("  " + p.sourcePointers.compactMap { String(format: "%08X", $0) }.joined(separator: " "))
+    */
+    
+    let dataStart = Int(patch.tonePointer)
+    let sourceCount: UInt8 = dataPool.scanValue(start: dataStart + Bank.sourceCountOffset, length: UInt8.byteWidth)
+    let dataSize = Bank.commonDataSize + Bank.sourceDataSize * Int(sourceCount) + Bank.additiveWaveKitSize * patch.additiveWaveKitCount
+    let dataEnd = dataStart + dataSize
+    let data = dataPool.subdata(in: dataStart..<dataEnd)
+    
+    let nameStart = Bank.nameOffset
+    let nameEnd = nameStart + Bank.nameSize
+    let nameData = data.subdata(in: nameStart..<nameEnd)
+
+    var name = ""
+    // ASCII is probably the closest encoding
+    if let nameString = String(bytes: nameData, encoding: .ascii) {
+        // Copy the characters except the NUL bytes
+        for ch in nameString {
+            if ch != "\0" {
+                name.append(ch)
+            }
+        }
+    }
+    //print(String(format: "%03d %s", index, name))
+    print(index, name)
+    
+    
 }
 
