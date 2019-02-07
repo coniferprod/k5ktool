aa5d6e7f4e5d7c72636627b808587c5aaf06bfbf
Jere Käpyaho <jere@coniferproductions.com>
2019-02-07 17:58:18 +0200

    Updated tests

--- src/github.com/coniferprod/k5ktool/bank/bank_test.go
+++ src/github.com/coniferprod/k5ktool/bank/bank_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestGetEffectType(t *testing.T) {
 	data := []byte{0x11, 0x05, 0x50, 0x1F, 0x00, 0x00}
-	effect := getEffect(data)
+	effect := newEffect(data)
 	if effect.EffectType != 6 {
 		t.Error("Effect type should be 6")
 	}
@@ -12,7 +12,7 @@ func TestGetEffectType(t *testing.T) {
 
 func TestGetEffectName(t *testing.T) {
 	data := []byte{0x11, 0x05, 0x50, 0x1F, 0x00, 0x00}
-	effect := getEffect(data)
+	effect := newEffect(data)
 	name := effect.Description()
 	if name != "Stereo Delay" {
 		t.Error("Effect name should be 'Stereo Delay'")
