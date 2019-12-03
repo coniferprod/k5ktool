df7925b90e0171bc531cbf8e14d37fb4d8d5a729
Jere Käpyaho <jere@coniferproductions.com>
2019-12-03 07:11:18 +0200

    Use KSynthLib, refactor command line processing

--- Options.cs
+++ Options.cs
@@ -3,12 +3,43 @@ using CommandLine;
 namespace K5KTool
 {
     [Verb("create", HelpText="Create a new patch.")]
-    class CreateOptions
+    public class CreateOptions
     {
         [Option]
         public string PatchType { get; set; }
         
         [Option]
+        public string OutputFileName { get; set; }
+    }
+
+    [Verb("list", HelpText = "List contents of bank.")]
+    public class ListOptions
+    {
+        [Option('f', "filename", Required = true, HelpText = "Input file to be processed.")]
+        public string FileName { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "Output file format ('text' or 'html')")]
+        public string Output { get; set; }
+    }
+
+    [Verb("dump", HelpText = "Dump contents of bank.")]
+    public class DumpOptions
+    {
+        [Option('f', "filename", Required = true, HelpText = "Input file to be processed.")]
+        public string FileName { get; set; }
+    }
+
+    [Verb("report", HelpText = "Report on the specified bank.")]
+    public class ReportOptions
+    {
+        [Option('f', "filename", Required = true, HelpText = "Input file to be processed.")]
+        public string FileName { get; set; }
+    }
+
+    [Verb("init", HelpText = "Initialize a new bank.")]
+    public class InitOptions
+    {
+        [Option('o', "output", Required = true, HelpText = "Output file.")]
         public string OutputFileName { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,229 +2,48 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Text;
-using K5KLib;
+
 using CommandLine;
+
+using KSynthLib.Common;
+using KSynthLib.K5000;
 
 namespace K5KTool
 {
-    public struct SystemExclusiveHeader
-    {
-        public byte ManufacturerID;
-	    public byte Channel;
-	    public byte Function;
-	    public byte Group;
-	    public byte MachineID;
-	    public byte Substatus1;
-	    public byte Substatus2;
-
-        public override string ToString()
-        {
-            return String.Format("ManufacturerID = {0,2:X2}H, Channel = {1}, Function = {2,2:X2}H, Group = {3,2:X2}H, MachineID = {4,2:X2}H, Substatus1 = {5,2:X2}H, Substatus2 = {6,2:X2}H", ManufacturerID, Channel, Function, Group, MachineID, Substatus1, Substatus2);
-        }
-    }
-
-    public enum SystemExclusiveFunction
-    {
-        OneBlockDumpRequest = 0x00,
-        AllBlockDumpRequest = 0x01,
-        ParameterSend = 0x10,
-        TrackControl = 0x11,
-        OneBlockDump = 0x20,
-        AllBlockDump = 0x21,
-        ModeChange = 0x31,
-        Remote = 0x32,
-        WriteComplete = 0x40,
-        WriteError = 0x41,
-        WriteErrorByProtect = 0x42,
-        WriteErrorByMemoryFull = 0x44,
-        WriteErrorByNoExpandMemory = 0x45
-    }
-
-    public class PatchMap
-    {
-        const int Size = 19;  // bytes
-
-        public const int NumPatches = 128;
-
-        private bool[] include;
-
-        public PatchMap()
-        {
-            include = new bool[NumPatches];  // initialized to false by C#, right?
-        }
-
-        public PatchMap(byte[] data)
-        {
-            // TODO: Check that the data length matches
-
-            include = new bool[NumPatches];
-
-            StringBuilder buf = new StringBuilder();
-            for (int i = 0; i < data.Length; i++)
-            {
-                for (int j = 0; j < 7; j++)
-                {
-                    buf.Append(data[i].IsBitSet(j) ? "1" : "0");
-                }
-            }
-
-            string bitString = buf.ToString().Reversed();
-            for (int i = 0; i < bitString.Length; i++)
-            {
-                include[i] = bitString[i] == '1' ? true : false;
-            }
-        }
-
-        public PatchMap(bool[] incl)
-        {
-            include = new bool[NumPatches];
-            // TODO: Check that lengths match
-            for (int i = 0; i < incl.Length; i++)
-            {
-                include[i] = incl[i];
-            }
-        }
-
-        public bool this[int i]
-        {
-            get { return include[i]; }
-        }
-
-        public byte[] ToData()
-        {
-            StringBuilder buf = new StringBuilder();
-            for (int i = 0; i < include.Length; i++)
-            {
-                buf.Append(include[i] ? "1" : "0");
-                // each byte maps seven patches, and every 8th bit must be a zero
-                if (i % 8 == 0)
-                {
-                    buf.Append("0");
-                }
-            }
-            // The patches are enumerated starting from the low bits, so reverse the string.
-            string bitString = buf.ToString().Reversed();
-            // Now we have a long bit string. Slice it into chunks of eight bits to convert to bytes.
-            string[] parts = bitString.Split(8);
-            List<byte> data = new List<byte>();
-            foreach (string s in parts)
-            {
-                data.Add(Convert.ToByte(s, 2));
-            }
-            return data.ToArray();
-        }
-    }
 
     class Program
     {
-        // N.B. The length of the SysEx header varies by command!
-        private static int GetSystemExclusiveHeaderLength(SystemExclusiveFunction func)
-        {
-            if (func == SystemExclusiveFunction.OneBlockDumpRequest)
-            {
-                return 9;
-            }
-
-            // TODO: Add lengths for other commands
-
-            return 8;
-        }
-
-        private const int SystemExclusiveHeaderLength = 8;
-        private const byte SystemExclusiveTerminator = 0xF7;
-
-        static SystemExclusiveHeader GetSystemExclusiveHeader(byte[] data)
-        {
-            SystemExclusiveHeader header;
-            // data[0] is the SysEx identifier F0H
-            header.ManufacturerID = data[1];
-            header.Channel = data[2];
-		    header.Function = data[3];
-		    header.Group = data[4];
-		    header.MachineID = data[5];
-		    header.Substatus1 = data[6];
-		    header.Substatus2 = data[7];
-            return header;
-        }
-
         public const int SinglePatchCount = 128;  // banks A and D have 128 patches each
         public const int AdditiveWaveKitSize = 806;
         public const int SourceCountOffset = 51;
 
         static int Main(string[] args)
         {
-            var parserResult = Parser.Default.ParseArguments<CreateOptions>(args);
-
-            if (args.Length < 2)
-            {
-                System.Console.WriteLine("Usage: K5KTool cmd filename.syx");
-                return 1;
-            }
-
-            string command = args[0];
-            string fileName = args[1];
-            string patchName = "";
-            if (args.Length > 2)
-            {
-                patchName = args[2];
-            }
-
-            byte[] fileData = File.ReadAllBytes(fileName);
-            System.Console.WriteLine($"SysEx file: '{fileName}' ({fileData.Length} bytes)");
-
-            List<byte[]> messages = Util.SplitBytesByDelimiter(fileData, SystemExclusiveTerminator);
-            System.Console.WriteLine($"Got {messages.Count} messages");
-
-            foreach (byte[] message in messages)
-            {
-                SystemExclusiveHeader header = GetSystemExclusiveHeader(message);
-                // TODO: Check the SysEx file header for validity
-
-                int headerLength = GetSystemExclusiveHeaderLength(SystemExclusiveFunction.OneBlockDumpRequest);
-
-                // Extract the patch bytes (discarding the SysEx header)
-                int dataLength = message.Length - headerLength;
-                //System.Console.WriteLine($"data length = {dataLength}");
-                byte[] data = new byte[dataLength];
-                Array.Copy(message, headerLength, data, 0, dataLength);
-
-                SystemExclusiveFunction function = (SystemExclusiveFunction) header.Function;
-                if (function != SystemExclusiveFunction.OneBlockDump)
-                {
-                    System.Console.WriteLine($"This is not a block single dump: {header.ToString()}");
-                    break;
-                }
-
-                if (command.Equals("list"))  // just list patch name(s)
-                {
-                    // At first use only common data so that we can take a look without parsing everything:
-                    byte[] commonData = new byte[CommonSettings.DataSize + Source.DataSize];
-                    Buffer.BlockCopy(data, 0, commonData, 0, CommonSettings.DataSize + Source.DataSize);
-                    //System.Console.WriteLine(Util.HexDump(commonData));
-                    K5KLib.Single single = new K5KLib.Single(commonData);
-                    System.Console.WriteLine(single.Common.Name);
-                }
-                else if (command.Equals("dump"))   // show all patch information
-                {
-                    System.Console.WriteLine(String.Format("Original data (length = {0} bytes):\n{1}", data.Length, Util.HexDump(data)));
-                    K5KLib.Single single = new K5KLib.Single(data);
-                    System.Console.WriteLine(single.ToString());
-                }
-                else if (command.Equals("create"))
-                {
-                    K5KLib.Single single = NewSinglePatch("NewSound");
-                    byte[] singleData = single.ToData();
-                    System.Console.WriteLine(String.Format("Generated single data size: {0} bytes", singleData.Length));
-                    System.Console.WriteLine(Util.HexDump(singleData));
-                }
-            }
+            var parserResult = Parser.Default.ParseArguments<CreateOptions, ListOptions, DumpOptions, ReportOptions, InitOptions>(args);
+            parserResult.MapResult(
+                (CreateOptions opts) => RunCreateAndReturnExitCode(opts),
+                (ListOptions opts) => RunListAndReturnExitCode(opts),
+                (DumpOptions opts) => RunDumpAndReturnExitCode(opts),
+                (ReportOptions opts) => RunReportAndReturnExitCode(opts),
+                (InitOptions opts) => RunInitAndReturnExitCode(opts),
+                errs => 1
+            );
 
             return 0;
         }
 
-        static K5KLib.Single NewSinglePatch(string patchName)
+        public static int RunCreateAndReturnExitCode(CreateOptions opts)
         {
-            K5KLib.Single single = new K5KLib.Single();
+            SinglePatch single = NewSinglePatch("NewSound");
+            byte[] singleData = single.ToData();
+            Console.WriteLine(String.Format("Generated single data size: {0} bytes", singleData.Length));
+            Console.WriteLine(Util.HexDump(singleData));
+            return 0;
+        }
+
+        static SinglePatch NewSinglePatch(string patchName)
+        {
+            SinglePatch single = new SinglePatch();
 
             single.Common.Name = patchName;
             single.Common.Volume = 115;
@@ -407,6 +226,132 @@ namespace K5KTool
             single.Sources[1].DCA.VelocitySensitivity.ReleaseTime = 0;
 
             return single;
+        }
+
+        public static int RunListAndReturnExitCode(ListOptions opts)
+        {
+            Console.WriteLine("List");
+
+            string fileName = opts.FileName;
+            byte[] message = File.ReadAllBytes(fileName);
+            string namePart = new DirectoryInfo(fileName).Name;
+            DateTime timestamp = File.GetLastWriteTime(fileName);
+            string timestampString = timestamp.ToString("yyyy-MM-dd hh:mm:ss");
+            Console.WriteLine($"System Exclusive file: '{namePart}' ({timestampString}, {message.Length} bytes)");
+
+            SystemExclusiveHeader header = new SystemExclusiveHeader(message);
+            // TODO: Check the SysEx file header for validity
+
+            // Extract the patch bytes (discarding the SysEx header and terminator)
+            int dataLength = message.Length - SystemExclusiveHeader.DataSize - 1;
+            //System.Console.WriteLine($"data length = {dataLength}");
+            byte[] data = new byte[dataLength];
+            Array.Copy(message, SystemExclusiveHeader.DataSize, data, 0, dataLength);
+
+            // TODO: Split the data into chunks representing single, multi, drum, and effect data
+            //Console.WriteLine(String.Format("Total data length = {0} bytes", data.Length));
+
+            string outputFormat = opts.Output;
+            if (outputFormat.Equals("text"))
+            {
+                Console.WriteLine(MakeTextList(data, namePart));
+                return 0;
+            }
+            else if (outputFormat.Equals("html"))
+            {
+                Console.WriteLine(MakeHtmlList(data, namePart));
+                return 0;
+            }
+            else 
+            {
+                Console.WriteLine(String.Format($"Unknown output format: '{outputFormat}'"));
+                return -1;
+            }
+        }
+
+        private static string MakeTextList(byte[] data, string title)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("SINGLE patches:\n");
+
+            return sb.ToString();
+        }
+
+        private static string MakeHtmlList(byte[] data, string title)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            return sb.ToString();
+        }
+
+        public static int RunDumpAndReturnExitCode(DumpOptions opts)
+        {
+            Console.WriteLine("Dump");
+
+            string fileName = opts.FileName;
+            byte[] fileData = File.ReadAllBytes(fileName);
+
+            return 0;
+        }
+
+        public static int RunReportAndReturnExitCode(ReportOptions opts)
+        {
+            Console.WriteLine("Report");
+
+            string fileName = opts.FileName;
+            byte[] fileData = File.ReadAllBytes(fileName);
+            Console.WriteLine($"SysEx file: '{fileName}' ({fileData.Length} bytes)");
+
+            List<byte[]> messages = Util.SplitBytesByDelimiter(fileData, 0xf7);
+            Console.WriteLine($"Got {messages.Count} messages");
+
+            foreach (byte[] message in messages)
+            {
+                ProcessMessage(message);
+            }
+
+            return 0;
+        }
+
+        private static void ProcessMessage(byte[] message)
+        {
+            SystemExclusiveHeader header = new SystemExclusiveHeader(message);
+
+            Console.WriteLine("{0}", header);
+            Dictionary<SystemExclusiveFunction, string> functionNames = new Dictionary<SystemExclusiveFunction, string>()
+            {
+                { SystemExclusiveFunction.AllPatchDataDump, "All Patch Data Dump" },
+                { SystemExclusiveFunction.AllPatchDumpRequest, "All Patch Data Dump Request" },
+                { SystemExclusiveFunction.BlockPatchDataDump, "Block Patch Data Dump" },
+                { SystemExclusiveFunction.BlockPatchDumpRequest, "Block Patch Data Dump Request" },
+                { SystemExclusiveFunction.EditBufferDump, "Edit Buffer Dump" },
+                { SystemExclusiveFunction.OnePatchDataDump, "One Patch Data Dump" },
+                { SystemExclusiveFunction.OnePatchDumpRequest, "One Patch Data Dump Request" },
+                { SystemExclusiveFunction.ParameterSend, "Parameter Send" },
+                { SystemExclusiveFunction.ProgramChange, "Program Change" },
+                { SystemExclusiveFunction.WriteComplete, "Write Complete" },
+                { SystemExclusiveFunction.WriteError, "Write Error" },
+                { SystemExclusiveFunction.WriteErrorNoCard, "Write Error (No Card)" },
+                { SystemExclusiveFunction.WriteErrorProtect, "Write Error (Protect)" }
+            };
+
+            SystemExclusiveFunction function = (SystemExclusiveFunction)header.Function;
+            string functionName = "";
+            if (functionNames.TryGetValue(function, out functionName))
+            {
+                Console.WriteLine("Function = {0}", functionName);
+            }
+            else
+            {
+                Console.WriteLine("Unknown function: {0}", function);
+            }
+
+        }
+
+        public static int RunInitAndReturnExitCode(InitOptions opts)
+        {
+            Console.WriteLine("Init");
+            return 0;
         }
 
         static Dictionary<string, HarmonicEnvelope> HarmEnv = new Dictionary<string, HarmonicEnvelope>()
